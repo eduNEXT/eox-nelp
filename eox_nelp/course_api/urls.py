d7cf3feb300cb2cde2176f57c65fc000b81efa86
Johan Castiblanco <johan.castiblanco@edunext.co>
2023-02-09 16:12:06 -0500

    refactor!: change unnecessary file for course api
    feat: remove unnecesary course_id endpoint
    
    ALso remove some extra unused imports.
    
    feat: add backend for course-api
    
    Revert "feat: add backend for course-api"
    
    This reverts commit cc4b83cb9cc1147ee6d3e924abe7e95c239f083d.

--- eox_nelp/course_api/urls.py
+++ eox_nelp/course_api/urls.py
@@ -4,10 +4,9 @@ Course API URLs
 from django.conf import settings
 from django.conf.urls import url
 
-from .views import CourseDetailView, CourseIdListView, CourseListView
+from .views import CourseDetailView, CourseListView
 
 urlpatterns = [
     url(r'^v1/courses/$', CourseListView.as_view(), name="course-list"),
     url(r'^v1/courses/{}'.format(settings.COURSE_KEY_PATTERN), CourseDetailView.as_view(), name="course-detail"),
-    url(r'^v1/course_ids/$', CourseIdListView.as_view(), name="course-id-list"),
 ]

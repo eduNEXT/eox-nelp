5243c4172f24d0f75d2fd058a789e3830e03960b
Johan Castiblanco <johan.castiblanco@edunext.co>
2023-03-15 17:14:46 -0500

    feat: add signal for post save model
    https://github.com/eduNEXT/edx-platform/blob/b1880cb549ab9fdbb450e8d31a28ff5f394b9f42/lms/djangoapps/gating/signals.py#L30

---
+++ eox_nelp/signals.py
@@ -0,0 +1,15 @@
+"""
+Signal handlers for the eox-nelp djangoapp
+"""
+from completion.models import BlockCompletion
+from django.db import models
+from django.dispatch import receiver
+
+
+@receiver(models.signals.post_save, sender=BlockCompletion)
+def send_completion_progress_2_futurex(**kwargs):
+    instance = kwargs['instance']
+    course_id = str(instance.context_key)
+    block_id = str(instance.block_key)
+    user_id = instance.user_id
+    pass

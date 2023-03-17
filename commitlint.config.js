7c6427ff75b897286296544291db8d6760f3f173
Johan Castiblanco <johan.castiblanco@edunext.co>
2023-03-17 12:04:59 -0500

    fix: add infinite boody and footer length for urls
    This allow to add incredible long urls to keep them as
    a warning: `1`
    https://commitlint.js.org/#/reference-rules
    eg:
    https://github.com/eduNEXT/eox-nelp/commit/d915ff9dfc9152b3b2087258a2e57380685a79fb#diff-2b1ae4316086c0ebb213227790133a792300e1cef03b0afc067b23d3399ea5ca

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	parserPreset: 'conventional-changelog-conventionalcommits',
 	rules: {
 		'body-leading-blank': [1, 'always'],
-		'body-max-line-length': [2, 'always', 100],
+		'body-max-line-length': [1, 'always', 100],
 		'footer-leading-blank': [1, 'always'],
-		'footer-max-line-length': [2, 'always', 100],
+		'footer-max-line-length': [1, 'always', 100],
 		'header-max-length': [2, 'always', 100],
 		'subject-case': [
 			2,

74447364c2f8d184f5c9950cf98f6b8dcb60c13b
Johan Castiblanco <johan.castiblanco@edunext.co>
2023-04-14 14:27:01 -0500

    feat: checkpoint with celery beat comptibility

--- eox_nelp/tasks.py
+++ eox_nelp/tasks.py
@@ -2,10 +2,11 @@
 
 from celery import shared_task
 import logging
+logger = logging.getLogger(__name__)
 
 @shared_task
 def log_message():
     """
     Task to log a message every 5 minutes
     """
-    logging.info("This is a log message.")  # Log your desired message here
+    logger.info("This is a log message.")  # Log your desired message here

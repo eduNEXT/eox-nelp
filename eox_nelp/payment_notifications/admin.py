2b7ab8e85300aebbbae6a81f80f556463ab13241
andrey-canon <andrey.canon@edunext.co>
2023-07-26 10:47:34 -0500

    feat: add list search and filter fields

--- eox_nelp/payment_notifications/admin.py
+++ eox_nelp/payment_notifications/admin.py
@@ -6,5 +6,30 @@ from eox_nelp.payment_notifications.models import PaymentNotification
 class PaymentNotificationAdmin(admin.ModelAdmin):
     """Admin class for PaymentNotification."""
 
+    list_display = (
+        "cdtrans_lms_user_id",
+        "cdtrans_username",
+        "cdtrans_email",
+        "cdtrans_course_id",
+        "cdtrans_response_id",
+        "cdtrans_amount",
+    )
+    search_fields = (
+        "cdtrans_lms_user_id",
+        "cdtrans_username",
+        "cdtrans_email",
+        "cdtrans_course_id",
+        "cdtrans_response_id",
+        "cdtrans_amount",
+    )
+    list_filter = (
+        "cdtrans_lms_user_id",
+        "cdtrans_username",
+        "cdtrans_email",
+        "cdtrans_course_id",
+        "cdtrans_response_id",
+        "cdtrans_amount",
+    )
+
 
 admin.site.register(PaymentNotification, PaymentNotificationAdmin)

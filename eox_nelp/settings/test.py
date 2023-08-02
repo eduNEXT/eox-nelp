d12faa2b85365693fe1933d0830b090cca868dd5
andrey-canon <andrey.canon@edunext.co>
2023-08-02 17:16:17 -0500

    feat: add certificates api client
    https://edunext.atlassian.net/browse/FUTUREX-478

--- eox_nelp/settings/test.py
+++ eox_nelp/settings/test.py
@@ -36,6 +36,9 @@ def plugin_settings(settings):  # pylint: disable=function-redefined
     settings.FUTUREX_API_CLIENT_ID = 'my-test-client-id'
     settings.FUTUREX_API_CLIENT_SECRET = 'my-test-client-secret'
     settings.FUTUREX_NOTIFY_SUBSECTION_SUBJECT_MESSAGE = DEFAULT_FUTUREX_NOTIFY_SUBSECTION_SUBJECT_MESSAGE  # noqa: F405
+    settings.EXTERNAL_CERTIFICATES_API_URL = 'https://testing.com'
+    settings.EXTERNAL_CERTIFICATES_API_CLIENT_ID = 'my-test-client-id'
+    settings.EXTERNAL_CERTIFICATES_API_CLIENT_SECRET = 'my-test-client-secret'
 
 
 SETTINGS = SettingsClass()

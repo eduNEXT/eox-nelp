b3411ffb644c3314dc6118bd499a152ae9256e7b
Andrey Cañon <andrey.canon@edunext.co>
2024-05-21 14:22:04 -0500

    feat: add ping, cdd and ead pipeline steps (#159)
    * feat: add ping, cdd and ead pipeline steps

--- eox_nelp/pearson_vue/pipeline.py
+++ eox_nelp/pearson_vue/pipeline.py
@@ -8,9 +8,14 @@ Functions:
     get_user_data(data: dict) -> dict: Retrieves and processes user data.
 """
 import phonenumbers
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
+from eox_nelp.api_clients.pearson_rti import PearsonRTIApiClient
 from eox_nelp.edxapp_wrapper.student import anonymous_id_for_user
+from eox_nelp.pearson_vue.constants import PAYLOAD_CDD, PAYLOAD_EAD, PAYLOAD_PING_DATABASE
+from eox_nelp.pearson_vue.utils import update_xml_with_dict
 
 User = get_user_model()
 
@@ -59,10 +64,182 @@ def get_user_data(user_id, **kwargs):  # pylint: disable=unused-argument
             "email": user.email,
             "address": profile.mailing_address,
             "city": profile.city,
-            "country": profile.country.code,
+            "country": profile.country.alpha3,
             "phone_number": phone,
             "phone_country_code": phone_country_code,
             "mobile_number": phone,
             "mobile_country_code": phone_country_code,
         },
     }
+
+
+def check_service_availability(**kwargs):  # pylint: disable=unused-argument
+    """
+    Checks the availability of the Pearson VUE RTI service by performing a ping request.
+
+    This function creates a payload with authentication details and sends a ping request
+    to the Pearson VUE RTI service to verify its availability. If the service is not
+    available, it raises an exception.
+
+    Args:
+        **kwargs: Additional keyword arguments (currently not used).
+
+    Raises:
+        Exception: If the Pearson VUE RTI service is not available.
+    """
+    api_client = PearsonRTIApiClient()
+    payload = {
+        "soapenv:Envelope": {
+            "soapenv:Header": {
+                "wsse:Security": {
+                    "wsse:UsernameToken": {
+                        "wsse:Username": getattr(settings, "PEARSON_RTI_WSDL_USERNAME"),
+                        "wsse:Password": {
+                            "#text": getattr(settings, "PEARSON_RTI_WSDL_PASSWORD")
+                        },
+                    },
+                },
+            },
+        },
+    }
+    payload = update_xml_with_dict(PAYLOAD_PING_DATABASE, payload)
+    response = api_client.ping(payload)
+
+    if response.get("status", "failed") == "failed":
+        raise Exception("The pearson vue service is not available")  # pylint: disable=broad-exception-raised
+
+
+def import_candidate_demographics(profile_metadata, **kwargs):  # pylint: disable=unused-argument
+    """
+    Imports candidate demographics data into the Pearson VUE RTI system.
+
+    This function creates a payload with candidate demographic details and sends a request
+    to the Pearson VUE RTI service to import this information. The payload includes personal
+    information such as the candidate's name, email, address, phone number, and other details.
+    If the import request is not accepted, the function raises an exception.
+
+    Args:
+        **kwargs: A dictionary containing the following key-value pairs:
+            - anonymous_user_id (str): An anonymized identifier for the user.
+            - first_name (str): The candidate's first name.
+            - last_name (str): The candidate's last name.
+            - email (str): The candidate's email address.
+            - address (str): The candidate's mailing address.
+            - city (str): The city of the candidate's address.
+            - country (str): The country code of the candidate's address.
+            - phone_number (str): The candidate's phone number in national format.
+            - phone_country_code (str): The country code of the candidate's phone number.
+            - mobile_number (str): The candidate's mobile number in national format.
+            - mobile_country_code (str): The country code of the candidate's mobile number.
+
+    Raises:
+        Exception: If the Pearson VUE RTI service does not accept the import request.
+    """
+    api_client = PearsonRTIApiClient()
+    payload = {
+        "soapenv:Envelope": {
+            "soapenv:Header": {
+                "wsse:Security": {
+                    "wsse:UsernameToken": {
+                        "wsse:Username": getattr(settings, "PEARSON_RTI_WSDL_USERNAME"),
+                        "wsse:Password": {
+                            "#text": getattr(settings, "PEARSON_RTI_WSDL_PASSWORD")
+                        },
+                    },
+                },
+            },
+            "soapenv:Body": {
+                "sch:cddRequest": {
+                    "@clientCandidateID": f'NELC{profile_metadata["anonymous_user_id"]}',
+                    "@clientID": getattr(settings, "PEARSON_RTI_WSDL_CLIENT_ID"),
+                    "candidateName": {
+                        "firstName": profile_metadata["first_name"],
+                        "lastName": profile_metadata["last_name"],
+                    },
+                    "webAccountInfo": {
+                        "email": profile_metadata["email"],
+                    },
+                    "lastUpdate": timezone.now().strftime("%Y/%m/%d %H:%M:%S GMT"),
+                    "primaryAddress": {
+                        "address1": profile_metadata["address"],
+                        "city": profile_metadata["city"],
+                        "country": profile_metadata["country"],
+                        "phone": {
+                            "phoneNumber": profile_metadata["phone_number"],
+                            "phoneCountryCode": profile_metadata["phone_country_code"],
+                        },
+                        "mobile": {
+                            "mobileNumber": profile_metadata["mobile_number"],
+                            "mobileCountryCode": profile_metadata["mobile_country_code"],
+                        }
+                    }
+                },
+            },
+        },
+    }
+    payload = update_xml_with_dict(PAYLOAD_CDD, payload)
+    response = api_client.import_candidate_demographics(payload)
+
+    if response.get("status", "error") != "accepted":
+        # pylint: disable=broad-exception-raised
+        raise Exception("Error trying to process import candidate demographics request.")
+
+
+def import_exam_authorization(
+    profile_metadata,
+    exam_metadata,
+    transaccion_type="Add",
+    **kwargs
+):  # pylint: disable=unused-argument
+    """
+    Imports exam authorization data into the Pearson VUE RTI system.
+
+    This function creates  a payload with exam  authorization details and  sends a
+    request to the Pearson VUE RTI service to import this information. The payload
+    includes details such as the candidate ID, exam  series code, and  eligibility
+    appointment dates. If the  import request is not accepted, the function raises
+    an exception.
+
+    Args:
+        transaccion_type (str): The type of transaction for the authorization (default is "Add").
+        **kwargs: A dictionary containing the following key-value pairs:
+            - anonymous_user_id (str): An anonymized identifier for the user.
+
+    Raises:
+        Exception: If the Pearson VUE RTI service does not accept the import request.
+    """
+    api_client = PearsonRTIApiClient()
+    payload = {
+        "soapenv:Envelope": {
+            "soapenv:Header": {
+                "wsse:Security": {
+                    "wsse:UsernameToken": {
+                        "wsse:Username": getattr(settings, "PEARSON_RTI_WSDL_USERNAME"),
+                        "wsse:Password": {
+                            "#text": getattr(settings, "PEARSON_RTI_WSDL_PASSWORD")
+                        },
+                    },
+                },
+            },
+            "soapenv:Body": {
+                "sch:eadRequest": {
+                    "@clientID": getattr(settings, "PEARSON_RTI_WSDL_CLIENT_ID"),
+                    "@authorizationTransactionType": transaccion_type,
+                    "clientCandidateID": f'NELC{profile_metadata["anonymous_user_id"]}',
+                    "examAuthorizationCount": exam_metadata["exam_authorization_count"],
+                    "examSeriesCode": exam_metadata["exam_series_code"],
+                    "eligibilityApptDateFirst": timezone.now().strftime("%Y/%m/%d %H:%M:%S"),
+                    "eligibilityApptDateLast": (
+                        timezone.now() + timezone.timedelta(days=365)
+                    ).strftime("%Y/%m/%d %H:%M:%S"),
+                    "lastUpdate": timezone.now().strftime("%Y/%m/%d %H:%M:%S GMT"),
+                },
+            },
+        },
+    }
+    payload = update_xml_with_dict(PAYLOAD_EAD, payload)
+    response = api_client.import_exam_authorization(payload)
+
+    if response.get("status", "error") != "accepted":
+        # pylint: disable=broad-exception-raised
+        raise Exception("Error trying to process import exam authorization request.")

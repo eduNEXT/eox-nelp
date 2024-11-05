606b4eb9144893ad52f724ab9d8f1324a17f877f
Johan Castiblanco <johan.castiblanco@edunext.co>
2024-11-05 12:07:28 -0500

    feat: load user admin if possible in init file

--- eox_nelp/admin/__init__.py
+++ eox_nelp/admin/__init__.py
@@ -1,11 +1,17 @@
 """General admin module file.
 Register all the nelp admin models.
 """
+from importlib.util import find_spec
+
+from django.conf import settings
+
 from eox_nelp.admin.certificates import *  # noqa: F401
 from eox_nelp.admin.course_creators import *  # noqa: F401
 from eox_nelp.admin.student import *  # noqa: F401
-from eox_nelp.admin.user import *  # noqa: F401
 from eox_nelp.course_experience.admin import *  # noqa: F401
 from eox_nelp.notifications.admin import *  # noqa: F401
 from eox_nelp.payment_notifications.admin import *  # noqa: F401
 from eox_nelp.pearson_vue.admin import *  # noqa: F401
+
+if find_spec('eox_support') and 'eox_support.apps.EoxSupportConfig' in settings.INSTALLED_APPS:
+    from eox_nelp.admin.user import *  # noqa: F401
--- eox_nelp/admin/user.py
+++ eox_nelp/admin/user.py
@@ -3,30 +3,25 @@ This module defines the Django admin configuration for handling user model.
 
 Classes:
     NelpUserAdmin: Custom admin class for User model to include extra info fields like national_id.
+    UserExtraInfoInline: inline for extra info model
 """
-from importlib import import_module
-from importlib.util import find_spec
+
 
 from custom_reg_form.models import ExtraInfo
-from django.conf import settings
 from django.contrib.admin import StackedInline
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from eox_support.admin.user import SupportUserAdmin  # pylint: disable=import-error
 
 from eox_nelp.admin.register_admin_model import register_admin_model as register
 
-if find_spec('eox_support') and 'eox_support.apps.EoxSupportConfig' in settings.INSTALLED_APPS:
-    SupportUserAdmin = import_module("eox_support.admin.user").SupportUserAdmin
-else:
-    SupportUserAdmin = BaseUserAdmin
-
 User = get_user_model()
+
 
 class UserExtraInfoInline(StackedInline):
     """ Inline admin interface for Extra Info model. """
     model = ExtraInfo
     can_delete = False
-    verbose_name_plural = ('Extra info')
+    verbose_name_plural = 'Extra info'
 
 
 class NelpUserAdmin(SupportUserAdmin):
@@ -44,5 +39,4 @@ class NelpUserAdmin(SupportUserAdmin):
         return None
 
 
-if find_spec('eox_support') and 'eox_support.apps.EoxSupportConfig' in settings.INSTALLED_APPS:
-    register(User, NelpUserAdmin)
+register(User, NelpUserAdmin)

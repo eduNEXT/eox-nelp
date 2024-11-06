b3d700c0b7173e4ca2673d11319f1951571bde31
Johan Seto Kaiba <51926076+johanseto@users.noreply.github.com>
2024-11-06 16:59:02 -0500

    feat: add nelpUser admin (#230)
    * feat: add nelpUser admin
    
    This admin user adds:
    - national_id from extra_info instance in list_display
    - search by national_id
    
    chore
    
    * feat: add eox-support requirement
    
    * Revert "feat: add eox-support requirement"
    
    This reverts commit d69741bddeef634807f551db6300f565e1e730d8.
    
    * fix: import eox_support hack
    
    * fix: studio doesnt have eox-support entrypoint
    
    * feat: add national_id in third pos in list_display
    
    * feat: extrainfo inline option
    
    * feat: load user admin if possible in init file
    
    * feat: add testing options

---
+++ eox_nelp/admin/user.py
@@ -0,0 +1,41 @@
+"""
+This module defines the Django admin configuration for handling user model.
+
+Classes:
+    NelpUserAdmin: Custom admin class for User model to include extra info fields like national_id.
+    UserExtraInfoInline: inline for extra info model
+"""
+
+
+from custom_reg_form.models import ExtraInfo
+from django.contrib.admin import StackedInline
+from django.contrib.auth import get_user_model
+from eox_support.admin.user import SupportUserAdmin  # pylint: disable=import-error
+
+from eox_nelp.admin.register_admin_model import register_admin_model as register
+
+User = get_user_model()
+
+
+class UserExtraInfoInline(StackedInline):
+    """ Inline admin interface for Extra Info model. """
+    model = ExtraInfo
+    can_delete = False
+    verbose_name_plural = 'Extra info'
+
+
+class NelpUserAdmin(SupportUserAdmin):
+    """EoxNelp User admin class."""
+    list_display = SupportUserAdmin.list_display[:2] + ('user_national_id',) + SupportUserAdmin.list_display[2:]
+    search_fields = SupportUserAdmin.search_fields + ('extrainfo__national_id',)
+    inlines = SupportUserAdmin.inlines + (UserExtraInfoInline,)
+
+    def user_national_id(self, instance):
+        """Return national_id associated with the user extra_info instance."""
+        if getattr(instance, "extrainfo", None):
+            return instance.extrainfo.national_id
+
+        return None
+
+
+register(User, NelpUserAdmin)

1405c025563ab8bf17a18f0e7f15c606df4c770e
Andres Felipe Giraldo Malagon <andres.giraldo@edunext.co>
2025-02-12 16:00:16 -0500

    chore: remove unused futurex api client

--- eox_nelp/signals/tasks.py
+++ eox_nelp/signals/tasks.py
@@ -16,9 +16,9 @@ from django.utils import timezone
 from eox_core.edxapp_wrapper.enrollments import get_enrollment
 from eventtracking import tracker
 from nelc_api_clients.clients.certificates import ExternalCertificatesApiClient
+from nelc_api_clients.clients.futurex import FuturexApiClient
 from opaque_keys.edx.keys import UsageKey
 
-from eox_nelp.api_clients.futurex import FuturexApiClient
 from eox_nelp.api_clients.mt import MinisterOfTourismApiClient
 from eox_nelp.edxapp_wrapper.course_blocks import get_student_module_as_dict
 from eox_nelp.edxapp_wrapper.course_overviews import CourseOverview
@@ -61,7 +61,7 @@ def _post_futurex_progress(data):
         data (dict): dict to send to futurex enrollment-progress path.
     """
     api_client = FuturexApiClient()
-    response = api_client.enrollment_progress(data)
+    response = api_client.send_enrollment_progress(data)
 
     logger.info(
         "send_futurex_progress --- The data %s was sent to the futurex service host %s. The response was: %s",

c94df16d8179bda2332080d6e5e8a16cf8699366
andrey-canon <andrey.canon@edunext.co>
2025-03-28 11:57:11 -0500

    feat: add atlas translation to react components

--- eox_nelp/user_profile/frontend/src/components/UserProfileForm/index.jsx
+++ eox_nelp/user_profile/frontend/src/components/UserProfileForm/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { APP_INIT_ERROR, APP_READY, subscribe, initialize } from '@edx/frontend-platform';
-import { ProfileDataModal, messages as essentialsMessages } from '@edunext/frontend-essentials';
+import { ProfileDataModal } from '@edunext/frontend-essentials';
 import { AppProvider } from '@edx/frontend-platform/react';
 
+import messages from '../../../../../i18n';
 import './index.scss';
 
 function LaunchUserProfileForm() {
@@ -19,4 +20,4 @@ subscribe(APP_READY, () => {
   ReactDOM.render(<LaunchUserProfileForm />, document.getElementById('user-profile-data-form'));
 });
 
-initialize({ messages: [essentialsMessages] });
+initialize({ messages });
